d8004d6465a3cfb76b93f4beeb744e39471b4fec
aon132543 <thanapat.nan@ku.th>
2020-06-21 10:06:44 +0700

    computer add to server

--- cpe/settings.py
+++ cpe/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '73*ls_+(ytrsd3!gpcwxn&%7235g)jh^f^7fzk!z&2mqze7v=m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['128.199.112.137']
+ALLOWED_HOSTS = ['comengsrc.page', 'www.comengsrc.page']
 
 
 # Application definition
