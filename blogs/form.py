aba7b88f72ff76a7214f0a25401149690d90451e
aon132543 <thanapat.nan@ku.th>
2020-06-21 13:46:49 +0700

    1.0.0.95 Edit text

--- blogs/form.py
+++ blogs/form.py
@@ -7,6 +7,7 @@ class signUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
     password1 = forms.CharField(max_length=100)
+    password2 = forms.CharField(max_length=100)
     class Meta:
         model =User
         fields = ('first_name','last_name','username','password1','password2')
